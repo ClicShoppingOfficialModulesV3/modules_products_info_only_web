5718f9f7d52bbe23a963a7808d02e1306233fc38
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2018-07-11 15:09:14 -0400

    new module

---
+++ sources/template/Default/modules/modules_products_info/content/products_info_only_web.php
@@ -0,0 +1,11 @@
+<?php
+  use ClicShopping\OM\CLICSHOPPING;
+?>
+<div class="<?php echo $text_position; ?> col-md-<?php echo $content_width; ?>">
+  <div class="separator"></div>
+  <div style="float: <?php echo MODULE_PRODUCTS_INFO_ONLY_WEB_POSITION; ?>;">
+    <div class="moduleProductsInfoOnlyWeb">
+      <span class="moduleProductsInfoOnlyWeb"><h3><?php echo  CLICSHOPPING::getDef('text_products_only_web'); ?></h3></span>
+    </div>
+  </div>
+</div>
